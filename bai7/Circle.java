2c202b51db55dac051f5d90ba257dc79432048ba
QS5525 <quansayking@gmail.com>
2024-11-14 01:43:11 +0700

    Create Circle.java

---
+++ bai7/Circle.java
@@ -0,0 +1,18 @@
+package bai7;
+
+public class Circle implements GeometricObject{
+	protected double radius=1.0;
+	public Circle() {
+	}
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	@Override
+	public double getPerimeter() {
+		return 2*3.14*radius;
+	}
+	@Override
+	public double getArea() {
+		return 3.14*radius*radius;
+	}
+}
